39ba9366264f374729de5227a8dd479a7dcc902c
rahman5828 <165983937+rahman5828@users.noreply.github.com>
2025-08-15 17:00:28 +0530

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,8 @@
+package com.example.app;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        String message = "Hello, Jenkins + Maven!";
+        System.out.println(message);
+    }
+}
